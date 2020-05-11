9814c3ddab9f1c962071a2a95af618e409de439a
n.mekh <nikita.mekh@gmail.com>
2020-05-11 15:05:12 +0300

    HGCMS-24: initial commit

---
+++ src/components/com_jshopping/controllers/hutkigrosh.php
@@ -0,0 +1,47 @@
+<?php
+/*
+* @info     Платёжный модуль Hutkigrosh для JoomShopping
+* @package  hutkigrosh
+* @author   esas.by
+* @license  GNU/GPL
+*/
+defined('_JEXEC') or die();
+
+use esas\cmsgate\hutkigrosh\controllers\ControllerHutkigroshAlfaclick;
+use esas\cmsgate\hutkigrosh\controllers\ControllerHutkigroshNotify;
+use esas\cmsgate\joomshopping\CmsgateController;
+use esas\cmsgate\utils\Logger as HgLogger;
+
+require_once(dirname(dirname(__FILE__)) . '/payments/pm_hutkigrosh/init.php');
+
+class JshoppingControllerHutkigrosh extends CmsgateController
+{
+    /**
+     * Выставляет счет в альфаклик
+     */
+    function alfaclick()
+    {
+        try {
+            $controller = new ControllerHutkigroshAlfaclick();
+            $controller->process();
+        } catch (Throwable $e) {
+            HgLogger::getLogger("alfaclick")->error("Exception: ", $e);
+        }
+    }
+
+
+    /**
+     * Callback, который вызывает сам ХуткиГрош для оповещение об оплате счета в ЕРИП
+     * Тут выполняется дополнительная проверка статуса счета на шлюза и при необходимости изменение его статус заказа
+     * в локальной БД
+     */
+    function notify()
+    {
+        try {
+            $controller = new ControllerHutkigroshNotify();
+            $controller->process();
+        } catch (Throwable $e) {
+            HgLogger::getLogger("callback")->error("Exception:", $e);
+        }
+    }
+}
